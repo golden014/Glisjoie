dc315e30be4b4a28ddf0eafece93a47eec9a2190
golden <josuagolden14@gmail.com>
2023-04-11 14:14:28 +0700

    login view model ok (kyknya)

--- app/src/main/java/com/bluecactus/glisjoie/Model/UserModel.kt
+++ app/src/main/java/com/bluecactus/glisjoie/Model/UserModel.kt
@@ -3,12 +3,11 @@ package com.bluecactus.glisjoie.Model
 class UserModel(
     val userDocumentID: String,
     val email: String,
+    val password: String,
     val username: String,
     val status: String,
     val role: String,
     val profilePictureURL: String,
     ) {
-
-
 
 }
