d4b500426c25427f75f2ec3a06dc1cba9dd4ed1a
golden <josuagolden14@gmail.com>
2023-04-11 13:04:43 +0700

    user model init

---
+++ app/src/main/java/com/bluecactus/glisjoie/Model/UserModel.kt
@@ -0,0 +1,14 @@
+package com.bluecactus.glisjoie.Model
+
+class UserModel(
+    val userDocumentID: String,
+    val email: String,
+    val username: String,
+    val status: String,
+    val role: String,
+    val profilePictureURL: String,
+    ) {
+
+
+
+}
